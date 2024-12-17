740daa0835540943ec33b477b718b9da6e69d377
Pawan Kumar <pawanpk552@gmail.com>
2024-12-17 11:44:25 +0530

    responsive layput with media query

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+         <header>
+
+         </header>
+         <section>
+           <div className="col-left"></div>
+           <div className="col-right"></div>
+         </section>
+      </div>
+      <div className="container secound">
+      <section>
+           <div className="col-left"></div>
+           <div className="col-right"></div>
+      </section>
+      <footer>
+        
+      </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
